d16f512873af7c33048984356644f461e70fc160
Stefan Milovanovic <milovanovic.stefan@outlook.com>
2019-03-29 14:49:41 +0100

    counter with reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { Component } from "react";
-import { Flex } from "reflexbox";
-import logo from "./logo.svg";
-import "./App.css";
+import React, {Component, useReducer} from 'react';
+import {Flex} from 'reflexbox';
+import logo from './logo.svg';
+import './App.css';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      throw new Error();
+  }
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, {count: 0});
   return (
-    <Flex justify="center" align="center">
-      App
+    <Flex justify="center" align="center" style={{height: '800px'}}>
+      <div>
+        Count: {state.count}
+        <button onClick={() => dispatch({type: 'increment'})}>+</button>
+        <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+      </div>
     </Flex>
   );
 };
