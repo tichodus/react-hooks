c286bb12f518cb3ee46e1883642b657cb035845d
Stefan Milovanovic <milovanovic.stefan@outlook.com>
2019-03-29 14:09:14 +0100

    set my title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Flex } from "reflexbox";
 import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
-    <Flex justify="center" align="center">
-      App
+    <Flex justify="center">
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </Flex>
   );
 };
