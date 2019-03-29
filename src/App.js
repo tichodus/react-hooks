6c22d5e2bf66a61734f4c26f34b3b3451b351f5d
Stefan Milovanovic <milovanovic.stefan@outlook.com>
2019-03-29 14:15:20 +0100

    feat: class example

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import { Flex } from "reflexbox";
 import logo from "./logo.svg";
 import "./App.css";
 
-const App = () => {
-  return (
-    <Flex justify="center" align="center">
-      App
-    </Flex>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentDidMount() {
+    document.title = `You clicked ${this.state.count} times`;
+  }
+
+  componentDidUpdate() {
+    document.title = `You clicked ${this.state.count} times`;
+  }
+
+  render() {
+    return (
+      <Flex justify="center">
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+      </Flex>
+    );
+  }
+}
 
 export default App;
