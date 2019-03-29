a1d017cf75210051bf094a7823ac2d288d63e220
Stefan Milovanovic <milovanovic.stefan@outlook.com>
2019-03-29 14:06:05 +0100

    feat:counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import { Flex } from "reflexbox";
-import logo from "./logo.svg";
-import "./App.css";
 
-const App = () => {
-  return (
-    <Flex justify="center" align="center">
-      App
-    </Flex>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  render() {
+    return (
+      <Flex align="center" justify="center" style={{ height: "800px" }}>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+      </Flex>
+    );
+  }
+}
 
 export default App;
